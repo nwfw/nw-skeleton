91e076153d25dfe3ab049542f98c68ddab4e2735
Dino Ivankov <dinoivankov@gmail.com>
2017-11-29 16:27:29 +0100

    Support file editing for file viewer component

--- app-wrapper/components/global/file-viewer/file-viewer.js
+++ app-wrapper/components/global/file-viewer/file-viewer.js
@@ -91,27 +91,25 @@ exports.component = {
             this.data.code = await _appWrapper.fileManager.loadFile(this.data.file, false);
         }
         CodeMirror.modeURL = path.resolve(this.config.modeUrl);
+        let options = _.cloneDeep(this.data.cmOptions);
+        if (_.isUndefined(options.mode)){
+            options.mode = this.data.mode;
+        }
+        if (_.isUndefined(options.readOnly)){
+            options.readOnly = true;
+        }
+        if (_.isUndefined(options.theme) && _appWrapper.getConfig('theme').match(/dark/)){
+            options.theme = 'blackboard';
+        }
         if (this.data.mode){
             CodeMirror.requireMode(this.data.mode, async () => {
                 this.$nextTick(async () => {
-                    let options = _.cloneDeep(this.data.cmOptions);
-                    if (_.isUndefined(options.mode)){
-                        options.mode = this.data.mode;
-                    }
-                    if (_.isUndefined(options.readOnly)){
-                        options.readOnly = true;
-                    }
-                    if (_.isUndefined(options.theme) && _appWrapper.getConfig('theme').match(/dark/)){
-                        options.theme = 'blackboard';
-                    }
                     await _appWrapper.wait(1000);
                     this.editorInstance = CodeMirror.fromTextArea(this.$el.querySelector('.file-viewer-textarea'), options);
                 });
             });
         } else {
-            this.editorInstance = CodeMirror.fromTextArea(this.$el.querySelector('.file-viewer-textarea'), {
-                readOnly: true
-            });
+            this.editorInstance = CodeMirror.fromTextArea(this.$el.querySelector('.file-viewer-textarea'), options);
         }
     },
     methods: {
